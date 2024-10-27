49516d7a22b25482f3ac5b18b12aaff2a11df166
Sam <skwort@skwort.dev>
2024-10-27 23:10:01 +1000

    tests: Use custom root dir for integration tests
    This commit updates the integration tests to use a custom,
    randomly-generated root directory for the client and daemon.

--- tests/integration/test_client_daemon.py
+++ tests/integration/test_client_daemon.py
@@ -2,12 +2,17 @@ import pytest
 import subprocess
 import signal
 import time
+import string
+import random
+import shutil
 
 DAEMON_TARGET = "daemon"
 CLIENT_TARGET = "client"
 
 DAEMON_PATH = "./build/daemon"
 CLIENT_PATH = "./build/client"
+
+NAV_ROOT = "/tmp/nav-" + "".join(random.choices(string.ascii_letters, k=6))
 
 
 def make_clean():
@@ -29,7 +34,7 @@ def build_all():
 @pytest.fixture()
 def daemon():
     # Start the daemon process in the background
-    process = subprocess.Popen([DAEMON_PATH],
+    process = subprocess.Popen([DAEMON_PATH, "-d", NAV_ROOT],
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
 
@@ -48,14 +53,17 @@ def daemon():
     except subprocess.TimeoutExpired:
         process.kill()
 
+    shutil.rmtree(NAV_ROOT)
+
 
 def test_unregister_not_registered(daemon):
     """
-    Test unregistering an unregistered client. This should fail with a
-    non-zero return value.
+    Test unregistering an unregistered client. This should fail with a non-zero
+    return value.
     """
     pid = "123456"
-    client = subprocess.run([CLIENT_PATH, pid, "unregister"],
+    client = subprocess.run([CLIENT_PATH, "-d", NAV_ROOT, pid,
+                             "unregister"],
                             capture_output=True, text=True)
 
     assert client.returncode == 1
@@ -66,12 +74,14 @@ def test_register_unregister(daemon):
     Test registering and unregistering a client with the daemon.
     """
     pid = "123456"
-    client = subprocess.run([CLIENT_PATH, pid, "register"],
+    client = subprocess.run([CLIENT_PATH, "-d", NAV_ROOT, pid,
+                             "register"],
                             capture_output=True, text=True)
 
     assert client.returncode == 0
 
-    client = subprocess.run([CLIENT_PATH, pid, "unregister"],
+    client = subprocess.run([CLIENT_PATH, "-d", NAV_ROOT, pid,
+                             "unregister"],
                             capture_output=True, text=True)
 
     assert client.returncode == 0
