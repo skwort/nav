17578f087452dcaca41c4e6d6105215fd5aac319
Sam <skwort@skwort.dev>
2024-10-27 15:46:21 +1000

    Allow users to specify root dir in client
    This commit allows users to specify the root dir of the nav
    application, i.e. where the sockets and tagfile will be located.

--- src/client/main.c
+++ src/client/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
@@ -14,6 +15,7 @@ static int sfd;
 static char *uname;
 static struct sockaddr_un my_addr;
 static struct sockaddr_un nav_addr;
+static char rootdir[64] = {0};
 
 static void handler(int signo, siginfo_t *info, void* context)
 {
@@ -28,7 +30,7 @@ static void process_command(int argc, char** argv)
 {
     char buf[1024] = {0};
     int offset = 0;
-    char **ptr = argv + 1;
+    char **ptr = argv + 1;  /* Skip PID */
 
     while (*ptr != NULL) {
         offset += sprintf(buf + offset,  "%s ", *ptr);
@@ -49,6 +51,7 @@ static void process_command(int argc, char** argv)
 
 int main(int argc, char** argv)
 {
+    int opt;
     int err;
     struct sigaction sa;
 
@@ -56,14 +59,42 @@ int main(int argc, char** argv)
     sa.sa_sigaction = &handler;
     sigaction(SIGINT, &sa, NULL);
 
+    while ((opt = getopt(argc, argv, "vd:")) != -1) {
+        switch (opt) {
+        case 'd':
+            strncpy(rootdir, optarg, sizeof(rootdir));
+            break;
+        case 'v':
+            printf("nav client version 0\n");
+            exit(EXIT_SUCCESS);
+        default:
+            fprintf(stderr, "Usage: %s -d <directory> [command]\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (optind >= argc) {
+        fprintf(stderr, "Usage: %s -d <directory> [command]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     uname = get_username();
     if (uname == NULL) {
         LOG_ERR("Invalid user.");
         exit(EXIT_FAILURE);
     }
 
+    /* Setup root directory */
+    if (strlen(rootdir) > 0) {
+        if (!valid_path(rootdir))
+            exit(EXIT_FAILURE);
+    } else {
+        sprintf(rootdir, "/home/%s/.nav", uname);
+    }
+    LOG_INF("Using root nav directory '%s'", rootdir);
+
     nav_addr.sun_family = AF_UNIX;
-    sprintf(nav_addr.sun_path, "/home/%s/.nav/nav.sock", uname);
+    snprintf(nav_addr.sun_path, 108, "%s/nav.sock", rootdir);
 
     sfd = socket(AF_UNIX, SOCK_DGRAM, 0);
     if (sfd == -1) {
@@ -72,19 +103,24 @@ int main(int argc, char** argv)
     }
 
     my_addr.sun_family = AF_UNIX;
-    sprintf(my_addr.sun_path, "/home/sam/.nav/%s.sock", argv[1]);
+    snprintf(my_addr.sun_path, 108, "%s/%s.sock", rootdir, argv[optind]);
     err = bind(sfd, (struct sockaddr *) &my_addr, sizeof(my_addr));
     if (err == -1) {
         LOG_ERR("bind: %s", strerror(errno));
+        close(sfd);
         exit(EXIT_FAILURE);
     }
 
     err = connect(sfd, (struct sockaddr *)&nav_addr, sizeof(nav_addr));
     if (err == -1) {
         LOG_ERR("connect: %s '%s'", strerror(errno), nav_addr.sun_path);
+        close(sfd);
+        unlink(my_addr.sun_path);
         exit(EXIT_FAILURE);
     }
 
+    argc -= optind;
+    argv += optind;
     process_command(argc, argv);
 
     return 0;
